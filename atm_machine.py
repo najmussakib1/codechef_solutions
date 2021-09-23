4306c3c8c660807217795b612e93de99d7bb73c6
Md. Najmus Sakib <89391621+najmussakib1@users.noreply.github.com>
2021-09-23 12:14:33 +0600

    Update atm_machine.py

--- atm_machine.py
+++ atm_machine.py
@@ -1,10 +1,11 @@
-# cook your dish here
+# problem link: https://www.codechef.com/problems/ATM2
 for _ in range(int(input())):
     N, K = list(map(int,input().split(' ')))
     tk = list(map(int,input().split(' ')))
     ans =""
     for amount in tk:
         if amount<=K:
+            # checking if the amount is less than the initial balance of atm machine
             ans+='1'
             K-=amount
         else:
