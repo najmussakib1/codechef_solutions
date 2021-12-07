6271f29d02af40da47750b14b72d9be997d4e8e9
Md. Najmus Sakib <89391621+najmussakib1@users.noreply.github.com>
2021-12-07 20:46:41 +0600

    Add files via upload

---
+++ checkmate.py
@@ -0,0 +1,31 @@
+# cook your dish here
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    # xk,yk,x1,y1,x2,y2
+    xk, yk = list(map(int,input().split()))
+    x1, y1= list(map(int,input().split()))
+    x2, y2 = list(map(int,input().split()))
+    ans = "NO"
+    if not (xk==1 or xk==8 or yk==1 or yk==8):
+        ans = "NO"
+    else:
+        if xk==1:
+            if (x1==2 or x2==2) and y1!=y2:
+                #...
+                if abs(y1-yk)>1 and abs(y2-yk)>1:
+                    ans="YES"
+        elif xk==8:
+            if (x1==7 or x2==7) and y1!=y2:
+                if abs(y1-yk)>1 and abs(y2-yk)>1:
+                    ans="YES"
+        if yk==1:
+            if (y1==2 or y2==2) and x1!=x2:
+                if abs(x1-xk)>1 and abs(x2-xk)>1:
+                    ans="YES"
+                
+        elif yk==8:
+            if (y1==7 or y2==7) and x1!=x2:
+                if abs(x1-xk)>1 and abs(x2-xk)>1:
+                    ans="YES"
+    print(ans)
